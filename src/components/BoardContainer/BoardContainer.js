94e097e3bb0fd26bf3b952b62f0a2cb1edd9542c
Mark Young <markyaaronyoung@gmail.com>
2020-08-24 19:42:04 -0500

    conditionally display new boardform on boardcontainer

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 import setSingleBoard from '../SingleBoard/SingleBoard';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import authData from '../../helpers/data/authData';
 
 import boardsData from '../../helpers/data/boardsData';
+
+// create board
+// boardForm component
+// show boardform on a click
+//  need a button
+// on submit of form: save to firebase, make sure board shows up
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -16,6 +23,7 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
   componentDidMount() {
@@ -25,12 +33,16 @@ class BoardContainer extends React.Component {
   }
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
 
     const boardCard = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
     return (
-      <div className="card-columns">
+      <div>
+        <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}></button>
+    { formOpen ? <BoardForm /> : '' }
+         <div className="card-columns">
         {boardCard}
+      </div>
       </div>
     );
   }
---
+++ src/components/BoardForm/BoardForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class BoardForm extends React.Component {
+  render() {
+    return (
+      <div className="BoardForm">Here is a Board </div>
+    );
+  }
+}
+
+export default BoardForm;
