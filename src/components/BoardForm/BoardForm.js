cbdf16b07a0a42ac3dceb8bdc99aa0521a8cf96c
Mark Young <markyaaronyoung@gmail.com>
2020-08-24 20:06:53 -0500

    BoardForm changes on state

--- src/components/BoardForm/BoardForm.js
+++ src/components/BoardForm/BoardForm.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
+import authData from '../../helpers/data/authData';
+
 class BoardForm extends React.Component {
+  state = {
+    name: '',
+  }
+
+  changeNameEvent = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  }
+
+  saveBoardEvent = (e) => {
+    e.preventDefault();
+    const { name } = this.state;
+
+    const newBoard = {
+      name,
+      userId: authData.getUid(),
+    }
+     console.warn('here is a new board', newBoard);
+  }
+
   render() {
     return (
-      <div className="BoardForm">Here is a Board </div>
+      <form className="col-6 offset-3">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Board Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="boardName"
+            placeholder="Enter Board Name"
+            onChange={this.changeNameEvent}
+            />
+        </div>
+          <button type="submit" className="btn btn-primary" onClick={this.saveBoardEvent}>Save Board</button>
+      </form>
     );
   }
 }
